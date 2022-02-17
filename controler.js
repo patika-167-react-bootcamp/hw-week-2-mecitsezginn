e9e49bbe6256ab35fa1964c53ad5b46d7f9b2846
Mecit SEZGİN <76531620+mecitsezgin55@users.noreply.github.com>
2022-02-17 17:38:04 +0300

    revize 1: müşteri silme ve geri iade eklendi

--- controler.js
+++ controler.js
@@ -1,21 +1,41 @@
 
 const state = {
     userList: [],
+    historyList: []
 };
 
+
+// ********* state *********
 const setState = (stateName, newValue) => {
     state[stateName] = newValue;
     renderUserList();
 }
-const updateState = (custemerId,newValue) => {
-    state.userList.forEach((item) =>{
-        if(Number(item.id) === Number(custemerId)){
+const updateState = (custemerId, newValue) => {
+    state.userList.forEach((item) => {
+        if (Number(item.id) === Number(custemerId)) {
             return item.balance = Number(newValue)
         }
     });
     renderUserList();
 }
 
+const updateHistoryState = (historyId, myIsButtonActive) => {
+    state.historyList.forEach((item) => {
+        if (Number(item.id) === Number(historyId)) {
+            return item.isButtonActive = myIsButtonActive
+        }
+    });
+    renderHistory();
+}
+
+
+const setStateHistory = (stateName, newValue) => {
+    state[stateName] = newValue;
+    renderHistory();
+}
+//***************************
+
+// ********* render *********
 const renderUserList = () => {
     const subscribers1 = [
         document.getElementById("customers")
@@ -34,6 +54,19 @@ const renderUserList = () => {
     })
 }
 
+const renderHistory = () => {
+    const subscribers = [
+        document.getElementById("history"),
+    ]
+    subscribers.forEach((subscriber) => {
+        subscriber.innerHTML = ""
+        historyTransferList(state.historyList, subscriber)
+    })
+};
+
+//***************************
+
+// ********* add *********
 const addUser = () => {
     const userName = document.getElementById("userName").value;
     const userBalance = document.getElementById("userBalance").value;
@@ -56,59 +89,170 @@ const addUser = () => {
         // console.log(userName, userBalance, state.userList)
 
         const myMessage = userName + " müşterisi eklendi";
-        history(myMessage);
+        // historyList(myMessage);
+        addHistory(0, 0, 0,myMessage,false,false,myDate())
     }
-    
+
 }
 
+const addHistory = (sCustomerId, rCustomerId, sAmount, myMessage, myIsThereButton, myIsButtonActive,myDate) => {
+    let historyId = 0;
+    if (state.historyList.length === 0) {
+        historyId = 1;
+    }
+    else {
+        const lastIndex = state.historyList.length;
+        historyId = state.historyList[lastIndex - 1].id + 1;
+    }
+
+    setStateHistory("historyList", [
+        ...state.historyList,
+        {
+            id: historyId,
+            sendingCustomerId: Number(sCustomerId),
+            receivingCustomerId: Number(rCustomerId),
+            amount: Number(sAmount),
+            message: myMessage,
+            isThereButton: myIsThereButton,
+            isButtonActive: myIsButtonActive,
+            date: myDate
+        }
+    ])
+}
+//***************************
+
+// ********* remove user *********
+const removeUser = (userId) => {
+    let myMessage = "";
+    state.userList.forEach((item, index) => {
+        if (userId === item.id) {
+            myMessage = state.userList[index].name + " müşterisi silindi"
+            state.userList.splice(index, 1);
+        }
+    })
+
+    renderUserList();
+
+    addHistory("", "", "", myMessage,false,false,myDate())
+
+}
+//***************************
+
+// ********* show list *********
 const customerList = (list, subscriber) => {
     list.forEach((item) => {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("data-id", item.id);
-        newDiv.className = "d-flex justify-content-around border-bottom mt-2"
+        newDiv.className = "d-flex justify-content-between border-bottom mt-2"
         subscriber.appendChild(newDiv);
 
         const newP1 = document.createElement("p");
-        newP1.innerText = item.name ;
+        newP1.innerText = item.name;
         newP1.className = "m-0 p-0"
         newDiv.appendChild(newP1);
 
         const newP2 = document.createElement("p");
-        newP2.innerText = item.balance + " TL";
+        newP2.innerText = item.balance + " ₺";
         newP2.className = "m-0 p-0 font-weight-bold"
         newDiv.appendChild(newP2);
+
+        const button = document.createElement("button");
+        button.setAttribute("data-id", item.id);
+        button.innerText = "Sil";
+        button.className = "btn btn-danger btn-sm mb-1";
+        button.onclick = (() => {
+            removeUser(item.id)
+        })
+        newDiv.appendChild(button);
     })
 }
 
-const sendReceiveCustomerList = (list, subscriber) =>{
+const sendReceiveCustomerList = (list, subscriber) => {
 
     list.forEach((item) => {
-        if(subscriber.id === "sending-customer" && item.id === 1){
-            const sOption= document.createElement("option");
+        if (subscriber.id === "sending-customer" && item.id === 1) {
+            const sOption = document.createElement("option");
             sOption.innerText = "Gönderen Müşteri";
             subscriber.appendChild(sOption);
         }
-        if(subscriber.id === "receiving-customer" && item.id === 1){
-            const rOption= document.createElement("option");
+        if (subscriber.id === "receiving-customer" && item.id === 1) {
+            const rOption = document.createElement("option");
             rOption.innerText = "Alıcı Müşteri";
             subscriber.appendChild(rOption);
         }
-        const newOption= document.createElement("option");
+        const newOption = document.createElement("option");
         newOption.innerText = item.name;
         newOption.value = item.id;
         subscriber.appendChild(newOption);
     });
 }
 
+const myDate = () => {
+    let fullYear = new Date().toLocaleDateString();
+    let fullTime = new Date().toLocaleTimeString()
+
+    fullYear = fullYear.split(".")
+    return fullYear[2] + ":" + fullYear[1] + ":" + fullYear[0] + " " + fullTime
+}
+const historyList = (message) => {
+    const his = document.getElementById("history");
+    const newDiv = document.createElement("div");
+    newDiv.className = "d-flex border-bottom mt-2"
+    his.appendChild(newDiv);
+
+    const newP1 = document.createElement("p");
+    newP1.innerText = myDate() + " : ";
+    newP1.className = "m-0 p-0 pr-2 font-weight-bold"
+    newDiv.appendChild(newP1);
+
+    const newP2 = document.createElement("p");
+    newP2.innerText = message;
+    newP2.className = "m-0 p-0 pr-2"
+    newDiv.appendChild(newP2);
+}
+
+const historyTransferList = (list, subscriber) => {
+    list.forEach((item) => {
+        const newDiv = document.createElement("div");
+        newDiv.className = "d-flex border-bottom mt-2"
+        subscriber.appendChild(newDiv);
+
+        const newP1 = document.createElement("p");
+        newP1.innerText = item.date  + " : ";
+        newP1.className = "m-0 p-0 pr-2 font-weight-bold"
+        newDiv.appendChild(newP1);
+
+        const newP2 = document.createElement("p");
+        newP2.innerText = item.message;
+        newP2.className = "m-0 p-0 pr-2"
+        newDiv.appendChild(newP2);
+
+        if (item.isThereButton === true) {
+            let button = document.createElement("button");
+            button.innerText = "Geri Al";
+            button.className = "btn btn-warning btn-sm mb-1 ml-auto";
+            button.onclick = (() => {
+                undoTransfer(item.id)
+            })
+            if(item.isButtonActive === false){
+                button.disabled = true
+            }
+            newDiv.appendChild(button);
+        }
+    })
+}
+//***************************
+
+// ********* transfer *********
 const transfer = () => {
-    const transferAmount  = document.getElementById("money").value;
+    const transferAmount = document.getElementById("money").value;
     const sendingCustomerId = document.getElementById("sending-customer").value;
     const receivingCustomerId = document.getElementById("receiving-customer").value;
 
-    if(transferAmount === "" || sendingCustomerId === "Gönderen Müşteri" || receivingCustomerId === "Alıcı Müşteri"){
+    if (transferAmount === "" || sendingCustomerId === "Gönderen Müşteri" || receivingCustomerId === "Alıcı Müşteri") {
         alert("Müşteri bilgilerini giriniz")
     }
-    else{
+    else {
         var sendingCustomerInfo = state.userList.filter((item) => {
             if (Number(item.id) === Number(sendingCustomerId)) {
                 return true;
@@ -120,46 +264,61 @@ const transfer = () => {
                 return true;
             }
         })
-        if(Number(transferAmount) > 0){
-            if(Number(sendingCustomerInfo[0].balance) - Number(transferAmount) >= 0){
+        if (Number(transferAmount) > 0) {
+            if (Number(sendingCustomerInfo[0].balance) - Number(transferAmount) >= 0) {
                 updateState(sendingCustomerInfo[0].id, Number(sendingCustomerInfo[0].balance) - Number(transferAmount));
                 updateState(receivingCustomerInfo[0].id, Number(receivingCustomerInfo[0].balance) + Number(transferAmount));
-    
-                const myMessage = sendingCustomerInfo[0].name + " -> " + receivingCustomerInfo[0].name +" "+ String(transferAmount) + " TL gönderdi.";
-                history(myMessage);
+
+                const myMessage = sendingCustomerInfo[0].name + " -> " + receivingCustomerInfo[0].name + " " + String(transferAmount) + " ₺ gönderdi.";
+                addHistory(sendingCustomerInfo[0].id, receivingCustomerInfo[0].id, transferAmount,
+                    myMessage,true,true,myDate())
+                // historyTransferList(myMessage, state.historyList.length);
             }
-            else{
+            else {
                 alert("Yetersiz bakiye")
             }
             document.getElementById("money").value = "";
         }
-        else{
+        else {
             alert("Transfer ücreti 0'dan büyük olmalı")
         }
     }
 }
 
-const myDate = () =>{
-    let fullYear = new Date().toLocaleDateString();
-    let fullTime = new Date().toLocaleTimeString()
+// transferi geri alır
+const undoTransfer = (historyId) => {
+    state.historyList.forEach((hist) => {
+        if (Number(hist.id) === Number(historyId)) {
 
-    fullYear = fullYear.split(".")
-    return fullYear[2]+":"+fullYear[1]+":"+fullYear[0]+" "+fullTime
+            let sendingCust = state.userList.find((user) => {
+                if (Number(user.id) === hist.sendingCustomerId) {
+                    return true
+                }
+            })
+            let receivingCust = state.userList.find((user) => {
+                if (Number(user.id) === hist.receivingCustomerId) {
+                    return true
+                }
+            })
+
+            if (sendingCust && receivingCust) {
+                updateState(sendingCust.id, Number(sendingCust.balance) + Number(hist.amount));
+                updateState(receivingCust.id, Number(receivingCust.balance) - Number(hist.amount));
+
+                const myMessage = sendingCust.name + " <- " + receivingCust.name + " " + String(hist.amount) + " ₺ geri iade edildi.";
+                
+                addHistory(receivingCust.id, sendingCust.id, hist.amount,myMessage,false,false,myDate())
+                
+                updateHistoryState(historyId, false)
+                
+                    
+            }
+            else {
+                alert("Kullanıcı silindiği için işlem geri alınamaz")
+                updateHistoryState(historyId, false)
+            }
+        }
+    })
+    renderUserList()
 }
-const history = (message) =>{
-    
-    const his = document.getElementById("history");
-    const newDiv = document.createElement("div");
-    newDiv.className = "d-flex border-bottom mt-2"
-    his.appendChild(newDiv);
-
-    const newP1 = document.createElement("p");
-    newP1.innerText =  myDate() + " : ";
-    newP1.className = "m-0 p-0 pr-2 font-weight-bold"
-    newDiv.appendChild(newP1);
-
-    const newP2 = document.createElement("p");
-    newP2.innerText = message;
-    newP2.className = "m-0 p-0 pr-2"
-    newDiv.appendChild(newP2); 
-}
+//***************************
